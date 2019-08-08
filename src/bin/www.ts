b14a0b73c5394c828aee33ab7ca0f06b39bcbbe4
ohseunghyeon <postercolo@gmail.com>
2019-08-08 10:06:17 +0900

    Add dotenv

--- src/bin/www.ts
+++ src/bin/www.ts
@@ -1,6 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import app from '../app';
 
-const port = process.env.POST || 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
